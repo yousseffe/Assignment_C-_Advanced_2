261a4776a18b7cbe21c82f4e6f29a9ca16cfeeda
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-29 13:55:24 +0300

    Part2_Q2

--- Program.cs
+++ Program.cs
@@ -19,16 +19,33 @@ namespace Assignment_C__Advanced_2
                 end--;
             }
         }
+        public static List<int> GetEvens(List<int> list)
+        {
+            return list.Where(n => n % 2 == 0).ToList();
+        }
 
         public static void Main()
         {
-            ArrayList list = new ArrayList { 1, 2, 3, 4, 5 };
-            ReverseArrayList(list);
+            #region Q1
+            /*ArrayList list = new ArrayList { 1, 2, 3, 4, 5 };
+                ReverseArrayList(list);
 
-            foreach (var item in list)
-            {
-                Console.Write(item + " ");
-            }
+                foreach (var item in list)
+                {
+                    Console.Write(item + " ");
+                }*/
+            #endregion
+
+            #region Q2
+            /*List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9};
+                List<int> Evens = GetEvens(list);
+
+                foreach (int i in Evens)
+                {
+                    Console.Write(i + " "); ;
+                }*/ 
+            #endregion
+
         }
     }
 }
