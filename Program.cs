d84d91cbda08264667afe2dac365aa8a919063e4
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-29 13:46:03 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_C__Advanced_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
