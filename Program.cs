3330ed2dfcfbad2d762724102e4fc9bb18359c08
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-07-29 13:51:03 +0300

    Part2_Q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Assignment_C__Advanced_2
+﻿using System.Collections;
+
+namespace Assignment_C__Advanced_2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void ReverseArrayList(ArrayList list)
         {
-            Console.WriteLine("Hello, World!");
+            int start = 0;
+            int end = list.Count - 1;
+
+            while (start < end)
+            {
+                object temp = list[start];
+                list[start] = list[end];
+                list[end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        public static void Main()
+        {
+            ArrayList list = new ArrayList { 1, 2, 3, 4, 5 };
+            ReverseArrayList(list);
+
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
